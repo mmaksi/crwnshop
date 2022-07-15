54303693e52564e0d670b9dcecea438ec9aba0be
mmaksi <markmaksi@outlook.com>
2022-07-15 20:16:37 +0300

    updating readme file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is an e-commerce React web app that handles user authentication with the ability to show products of multiple categories, adding them to the cart with a checkout page to add/remove cart items before submitting the purchase order using Stripe API.
 [See Live](https://comshop.netlify.app/)
+
+![crwn-shop](https://i.ibb.co/3TGtdcj/crwnshop.png)
--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -9,8 +9,8 @@ export const selectCategories = createSelector(
   }
 );
 
-export const selectCategoriesMap = createSelector([selectCategories],(categories) =>
-  categories.reduce((acc, category) => {
+export const selectCategoriesMap = createSelector([selectCategories],(categoriesArray) =>
+  categoriesArray.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
