c68a58f0ac341d28aa8d6b3dabc32fafb947d294
mmaksi <markmaksi@outlook.com>
2022-09-16 14:52:33 +0300

    Applying responsive layout to the checkout table

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,40 +1,33 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
-import PaymentForm from '../../components/payment-form/payment-form.component'
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector'
-import "./checkout.styles.scss"
+import React from "react";
+import { useSelector } from "react-redux";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
+import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const cartItems = useSelector(selectCartItems)
-  const cartTotal = useSelector(selectCartTotal)
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
 
   return (
-    <div className='checkout-container'>
-      <div className='checkout-header'>
-        <div className="header-block">
-          <span>Product</span>
-        </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Price</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <span className="header-block">Product</span>
+        <span className="header-block">Description</span>
+        <span className="header-block">Quantity</span>
+        <span className="header-block">Price</span>
+        <span className="header-block">Remove</span>
       </div>
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <span className='total'>Total: ${cartTotal}</span>
-      <PaymentForm />
+      <span className="total">Total: ${cartTotal}</span>
+      {/* <PaymentForm /> */}
     </div>
   );
-}
+};
 
-export default Checkout
+export default Checkout;
